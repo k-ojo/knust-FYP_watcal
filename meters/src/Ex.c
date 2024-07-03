4b0bdd86672e04078d705d3522a40812548a6b80
jei <93317428+k-ojo@users.noreply.github.com>
2024-07-03 20:48:25 +0000

    Create Ex.c

---
+++ meters/src/Ex.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
+#include <mbus/mbus.h>
+
+#define SLAVE "/dev/pts/8" // Adjust this as necessary
+
+typedef enum {
+    RELAY_COMMAND,
+    DATA_REQUEST
+} FrameType;
+
+/**
+ * configure_serial_port - configuration for mbus protocol
+ * @fd: input file descriptor
+ */
+void configure_serial_port(int fd)
+{
+    struct termios options;
+    tcgetattr(fd, &options);
+    cfsetispeed(&options, B2400);
+    cfsetospeed(&options, B2400);
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+
+#ifdef CRTSCTS
+    options.c_cflag &= ~CRTSCTS;
+#endif
+    options.c_iflag &= ~(IXON | IXOFF | IXANY);
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    options.c_oflag &= ~OPOST;
+    tcsetattr(fd, TCSANOW, &options);
+}
+
+/**
+ * process_request - process incoming M-Bus frame
+ * @handle: M-Bus handle
+ * @frame: pointer to the M-Bus frame
+ */
+void process_request(mbus_handle *handle, mbus_frame *frame)
+{
+    // Process the incoming frame
+    printf("Received frame from master\n");
+    printf("Frame address: 0x%x\n", frame->address);
+
+    // Create a response frame
+    mbus_frame reply;
+    memset(&reply, 0, sizeof(mbus_frame));
+    
+    reply.start1 = MBUS_FRAME_ACK_START;
+    reply.control = MBUS_CONTROL_MASK_RSP_UD;
+    reply.address = frame->address;
+    reply.control_information = MBUS_CONTROL_INFO_DATA_SEND | MBUS_CONTROL_MASK_DIR_S2M;
+    reply.data_size = 1;
+    reply.data[0] = 0x06; // Sample response data
+    reply.stop = MBUS_FRAME_STOP;
+
+    // Send the response frame
+    if (mbus_send_frame(handle, &reply) == -1)
+    {
+        fprintf(stderr, "Failed to send response frame: %s\n", mbus_error_str());
+    }
+    else
+    {
+        printf("Sent response frame to address 0x%x\n", frame->address);
+    }
+}
+
+int main()
+{
+    // Open the slave serial port
+    int s_fd = open(SLAVE, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (s_fd == -1) {
+        perror("Failed to open slave port");
+        return -1;
+    }
+
+    // Configure the serial port
+    configure_serial_port(s_fd);
+
+    // Initialize M-Bus context
+    mbus_handle *handle = mbus_context_serial(SLAVE);
+    if (handle == NULL) {
+        perror("Failed to initialize M-Bus context");
+        close(s_fd);
+        return -1;
+    }
+
+    // Main loop to receive and process frames
+    while (1)
+    {
+        mbus_frame frame;
+        if (mbus_recv_frame(handle, &frame) == -1)
+        {
+            fprintf(stderr, "Failed to receive frame: %s\n", mbus_error_str());
+        }
+        else
+        {
+            process_request(handle, &frame);
+        }
+    }
+
+    // Clean up
+    mbus_context_free(handle);
+    close(s_fd);
+    return 0;
+}
