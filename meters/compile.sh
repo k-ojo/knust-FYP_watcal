50008d7582e1b10f3791350860f202da367a2672
K-ojo <gideonad4@gmail.com>
2024-07-04 13:38:32 +0000

    cleaned up

--- meters/compile.sh
+++ meters/compile.sh
@@ -7,12 +7,11 @@ CC=gcc
 CFLAGS="-lmbus -lm"
 
 #files for master object
-
-MASTER_FILES="src/try.c src/controllers.c"
+MASTER_FILES="src/master.c"
 
 #Files for slave object
 
-SLAVE_FILES="src/Ex.c src/controllers.c"
+SLAVE_FILES="src/slave.c"
 
 #compile master
 $CC -o master $MASTER_FILES $CFLAGS
--- meters/src/master.c
+++ meters/src/master.c
@@ -5,7 +5,7 @@
 void send_data_request(mbus_handle *handle, int address) {
     mbus_frame frame;
     memset(&frame, 0, sizeof(mbus_frame));
-    frame.start = MBUS_FRAME_LONG_START;
+    frame.start1 = MBUS_FRAME_LONG_START;
     frame.control = MBUS_CONTROL_MASK_REQ_UD2;
     frame.address = address;
     frame.checksum = 0x55; // Example checksum
@@ -16,7 +16,7 @@ void send_data_request(mbus_handle *handle, int address) {
 void send_relay_control(mbus_handle *handle, int address) {
     mbus_frame frame;
     memset(&frame, 0, sizeof(mbus_frame));
-    frame.start = MBUS_FRAME_LONG_START;
+    frame.start1 = MBUS_FRAME_LONG_START;
     frame.control = MBUS_CONTROL_MASK_REQ_UD1;
     frame.address = address;
     frame.checksum = 0x55; // Example checksum
@@ -32,15 +32,18 @@ int main() {
     // socat -d -d PTY,link=/tmp/virtualcom0 PTY,link=/tmp/virtualcom1
 
     // Initialize master on /tmp/virtualcom0
-    handle = mbus_connect_serial("/tmp/virtualcom0");
+    handle = mbus_context_serial("/tmp/virtualcom0");
+
     if (handle == NULL) {
         fprintf(stderr, "Failed to initialize M-Bus master\n");
         return 1;
     }
+    mbus_connect(handle);
 
     // Send a data request to the slave
+    printf("break\n");
     send_data_request(handle, 1);
-
+    printf("break\n");
     // Send a relay control command to the slave
     send_relay_control(handle, 1);
 
