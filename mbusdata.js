4e7544da6e937681f1a3994d3e693cb861ae3f83
K-ojo <gideonad4@gmail.com>
2024-07-08 00:58:50 +0000

    cleaned up

--- mbusdata.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const MbusDataSchema = new mongoose.Schema({
-    timestamp: { type: Date, default: Date.now },
-    value: Number
-});
-
-module.exports = mongoose.model('MbusData', MbusDataSchema);
