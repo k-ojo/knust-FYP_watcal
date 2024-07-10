b4eaf4453e81a5bd40bc8322df243769ac4752b0
k-ojo <gideonad4@gmail.com>
2024-07-10 09:10:35 +0000

    update main.h

--- meters/include/main.h
+++ meters/include/main.h
@@ -8,9 +8,11 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <mbus/mbus.h>
+#include <mosquitto.h>
 
-#define SLAVE "/dev/pts/8" // Change to the slave virtual port
-#define MASTER "/dev/pts/2" // Change to master port
+#define MQTT_HOST "localhost"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "meters/data"
 
 //#define R_COMMAND 0x53;
 //#define D_REQUEST 0x52;
@@ -19,15 +21,6 @@ typedef enum
 {	RELAY_COMMAND,
 	DATA_REQUEST
 }FrameType;
-
-/**
-* watcal_respond - slave action
-* @type- type, whether control or not
-*/
-typedef struct _watCal_respond
-{
-	int type;
-}watCal_respond;
 
 int watCal_set_frame(mbus_frame *, int);
 void watCal_proccess_frame();
